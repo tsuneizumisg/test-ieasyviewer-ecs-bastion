3e714d69f4353aee7bcdc8ff13bddb684e665f52
tsuneizumisg <142875615+tsuneizumisg@users.noreply.github.com>
2025-04-24 19:08:41 +0900

    Update check_login.sh

--- check_login.sh
+++ check_login.sh
@@ -2,13 +2,13 @@
 LOGIN_USER="exist"
 while :
 do
-    sleep 900
-    LOGIN_USER_NUMBER=`ps -ef | grep ssm-session-worker | grep -v grep | wc -l`
-    
-    if [ $LOGIN_USER_NUMBER != 0 ]; then
+    sleep 5
+    LOGIN_USER_NUMBER=$(ps -ef | grep ssm-session-worker | grep -v grep | wc -l)
+
+    if [ "$LOGIN_USER_NUMBER" -ne 0 ]; then
         LOGIN_USER="exist"
     else
-        if [ $LOGIN_USER = "no exist" ];then
+        if [ "$LOGIN_USER" = "no exist" ]; then
             exit 0
         else
             LOGIN_USER="no exist"
